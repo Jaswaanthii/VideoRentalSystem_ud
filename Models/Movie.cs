667c290e70793de58405cab19da38269570887fd
Mahstir <maxter.fa@gmail.com>
2022-02-01 16:13:56 +0000

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VideoRentalSystem.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Genre Genre { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public DateTime? DateAdded { get; set; }
+        public int NumberInStock { get; set; }
+    }
+}
