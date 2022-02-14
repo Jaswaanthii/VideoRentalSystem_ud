a69fcde45160e9283b2f85797a2718b8d2fc0c49
Amadou Drammeh <drammeha@coventry.ac.uk>
2022-02-14 12:03:17 +0000

    Fix error

--- Migrations/202202141159278_AddDueDate.Designer.cs
+++ Migrations/202202141159278_AddDueDate.Designer.cs
@@ -7,13 +7,13 @@ namespace VideoRentalSystem.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class AddNewPropertiesToMovieModel : IMigrationMetadata
+    public sealed partial class AddDueDate : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(AddNewPropertiesToMovieModel));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(AddDueDate));
         
         string IMigrationMetadata.Id
         {
-            get { return "202202130016200_AddNewPropertiesToMovieModel"; }
+            get { return "202202141159278_AddDueDate"; }
         }
         
         string IMigrationMetadata.Source
--- Migrations/202202141159278_AddDueDate.cs
+++ Migrations/202202141159278_AddDueDate.cs
@@ -3,18 +3,20 @@ namespace VideoRentalSystem.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class AddNewPropertiesToMovieModel : DbMigration
+    public partial class AddDueDate : DbMigration
     {
         public override void Up()
         {
-            AddColumn("dbo.Movies", "Quality", c => c.String());
             AddColumn("dbo.Movies", "Description", c => c.String());
+            AddColumn("dbo.Rentals", "NumberOfMoviesRented", c => c.Int(nullable: false));
+            AddColumn("dbo.Rentals", "DueDate", c => c.DateTime(nullable: false));
         }
         
         public override void Down()
         {
+            DropColumn("dbo.Rentals", "DueDate");
+            DropColumn("dbo.Rentals", "NumberOfMoviesRented");
             DropColumn("dbo.Movies", "Description");
-            DropColumn("dbo.Movies", "Quality");
         }
     }
 }
